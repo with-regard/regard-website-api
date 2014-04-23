7f78acc5a6141d1dae8e05d899984168d5ce88d2
David Blurton <david.blurton@gmail.com>
2014-04-23 16:38:21 +0100

    Still wrong

--- modules/api.js
+++ modules/api.js
@@ -48,7 +48,7 @@ app.post('/projects', function (req, res, next) {
     name: req.body.project.name
   });
 
-  Project.create(project).exec().then(function () {
+  Project.create(project).then(function () {
     res.json({
       "project": project
     });
