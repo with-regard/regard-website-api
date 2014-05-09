35f9a9717af2b3d5d3255d66bac0d93c50878435
David Blurton <david.blurton@gmail.com>
2014-05-09 12:23:08 +0100

    Allow credentials in cors
    Need to set allow origin to the origin domain. Credentials doens’t work
    with wildcard origin

--- modules/allowCors.js
+++ modules/allowCors.js
@@ -1,5 +1,6 @@
 module.exports = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
