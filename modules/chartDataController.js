99d558487a12d8d7c302ededde2a3c42dcb05b12
Neil Anderson <neil.t.anderson@gmail.com>
2014-05-22 15:38:32 +0100

    stop altering results inflight

--- modules/chartDataController.js
+++ modules/chartDataController.js
@@ -4,22 +4,15 @@ var DataStore = require('./regard-data-store.js');
 var router = express.Router();
 var dataStore = new DataStore('adobe', 'brackets');
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
   dataStore.runQuery('Adobe%20Brackets%20Extension').then(function (result) {
-    var chartData = JSON.parse(result).Results.map(function(chartElement) {
-      return {
-        name: chartElement['Extension'],
-        value: chartElement['UserIds']
-      }
-    });
-
     res.json({
       chartdata: [{
-          _id: '978207A8-D325-4817-AD98-79379893CDBF',
-          values: chartData
-        }]
+        _id: '978207A8-D325-4817-AD98-79379893CDBF',
+        values: JSON.parse(result).Results
+      }]
     });
-  });  
+  });
 });
 
 module.exports = router;
