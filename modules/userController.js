4c10863d2942cb9ea86cfce531e087483ac62f59
Neil Anderson <neil.t.anderson@gmail.com>
2014-05-23 15:28:55 +0100

    serve up an anonymous user

--- modules/userController.js
+++ modules/userController.js
@@ -5,11 +5,17 @@ var app = express();
 
 app.get('/users', function (req, res, next) {
   res.json({
-    "users": [req.user]
+    "users": [{
+      _id: "09f51f26-8211-4046-9295-cae7b7753371",
+      projects: ["537e0639444552182b524d45"],
+      isDeveloper: false
+    }]
   });
 });
 
 app.put('/users/:id', function (req, res, next) {
+  throw new Error('user cannot modify projects');
+  
   User.findById(req.params.id).exec().then(function (user) {
     user.projects = req.body.user.projects;
     user.save();
