5beab2e405305fae09bd937a1f4292a2e5c6c410
David Blurton <david.blurton@gmail.com>
2014-05-28 14:12:26 +0100

    Create the anonymous user server side instead

--- modules/userController.js
+++ modules/userController.js
@@ -4,12 +4,24 @@ var User = require('../schemas/userSchema.js');
 var app = express();
 
 app.get('/users', function (req, res, next) {
-  res.json({
-    "users": [req.user]
-  });
+  if (req.isAuthenticated()) {
+    res.json({
+      "users": [req.user]
+    });
+  } else {
+    res.json({
+      "users": [{
+        _id: "anonymous user"
+      }]
+    })
+  }
 });
 
 app.put('/users/:id', function (req, res, next) {
+  if (!req.isAuthenticated()) {
+    res.send(401, 'You must be logged in to join a project');
+  }
+
   User.findById(req.params.id).exec().then(function (user) {
     user.projects = req.body.user.projects;
     user.save();
