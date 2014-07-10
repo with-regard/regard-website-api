ee50da02ab0eb20d4c8c002959fc32ec072d3a16
David Blurton <david.blurton@gmail.com>
2014-07-10 11:14:50 +0100

    I don't even know why we have to do this

--- modules/organizationController.js
+++ modules/organizationController.js
@@ -2,6 +2,6 @@ var Organization = require('../schemas/organization.js');
 
 module.exports = {
   getOrganziationsForUser: function getOrganziationsForUser(userId) {
-    return Organization.find({ 'users': { $in: [userId] }}).exec();
+    return Organization.find({ 'users': { $in: [userId.toString()] }}).exec();
   }
 };
--- modules/userController.js
+++ modules/userController.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require('express');
 var User = require('../schemas/userSchema.js');
 var organizationController = require('./organizationController.js');
@@ -6,14 +8,12 @@ var app = express();
 
 app.get('/users', function (req, res, next) {
   if (req.isAuthenticated()) {
-    var user = req.user;
+    var user = JSON.parse(JSON.stringify(req.user));
 
-    organizationController.getOrganziationsForUser(user._id).then(function(organizations){
-      if(organizations.length === 0) {
-        organizations = [user._id];
-      }
-
-      user.organizations = organizations;
+    organizationController.getOrganziationsForUser(user._id).then(function(organizations) {
+      user.organizations = organizations.map(function(organization) {
+        return organization._id;
+      });
 
       res.json({
         "users": [user]
@@ -25,7 +25,7 @@ app.get('/users', function (req, res, next) {
       "users": [{
         _id: "anonymous user",
         isAnonymous: true,
-        projects: ["53997a47b12b52200c7e3b88"]
+        projects: ["53997a47b12b52200c7e3b88"] // change this to regard organization id
       }]
     });
   }
